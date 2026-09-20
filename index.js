9699b1f26e96dd50587950295e3d8df9fe0c8cc7
sakibulislam75 <sakibulislam1623@gmail.com>
2026-09-20 23:48:58 +0600

    feat: add get data endpoint

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient } = require("mongodb");
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
@@ -10,9 +11,36 @@ app.use(express.json());
 
 const port = process.env.PORT;
 
+const client = new MongoClient(
+  "mongodb+srv://mentora:unSTuVOC9rFgR6zf@cluster0.mvh6hdg.mongodb.net/?appName=Cluster0",
+);
+
 app.get("/", (req, res) => {
   res.send("server is running successfully!");
 });
+const run = async () => {
+  try {
+    await client.connect();
+    const db = await client.db("mentora");
+    const courseCollections = await db.collection("courses");
+
+    //fetch
+    app.get("/course", async (req, res) => {
+      try {
+        const result = await courseCollections.find({}).toArray();
+        res.send(result);
+      } catch (err) {
+        res.status(500).send({ message: "Failed to get courses" });
+      }
+    });
+    console.log("You successfully connected to MongoDB!");
+    return client;
+  } catch (err) {
+    console.dir(err);
+  }
+};
+
+run();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
